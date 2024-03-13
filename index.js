8976cb687843ef6bf3d2d97c6829e721f9c5981a
abhaydevelops <abhaysinghlko29@gmail.com>
2024-03-13 08:21:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var randomNumber1=Math.floor(Math.random()*3)+1;
+var imagsrc1="./assets/"+randomNumber1+".png";
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",imagsrc1);
