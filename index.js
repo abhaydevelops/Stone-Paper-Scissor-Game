5ef62d5d4855d07eeac14d8330b48ee488e1fb66
abhaydevelops <abhaysinghlko29@gmail.com>
2024-03-13 10:24:13 +0530

    updated css and js

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 var randomNumber1=Math.floor(Math.random()*3)+1;
-var imagsrc1="./assets/"+randomNumber1+".png";
+var imagsrc1="./assets/"+randomNumber1+".jpg";
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",imagsrc1);
+
+var randomNumber2=Math.floor(Math.random()*3)+1;
+var imagsrc2="./assets/"+randomNumber2+".jpg";
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",imagsrc2);
+
+
+if(randomNumber1==randomNumber2){
+    var result=document.querySelector("h3").innerHTML="Game Draw!!🏳️";
+}
+else if((randomNumber1==1)&&(randomNumber2==2)){
+    var result=document.querySelector("h3").innerHTML="PLAYER 2 Wins!🏆";
+}
+else if((randomNumber1==1)&&(randomNumber2==3)){
+    var result=document.querySelector("h3").innerHTML="PLAYER 1 Wins!🏆";
+}
+else if((randomNumber1==2)&&(randomNumber2==1)){
+    var result=document.querySelector("h3").innerHTML="PLAYER 1 Wins!🏆";
+}
+else if((randomNumber1==2)&&(randomNumber2==3)){
+    var result=document.querySelector("h3").innerHTML="PLAYER 2 Wins!🏆";
+}
+else if((randomNumber1==3)&&(randomNumber2==1)){
+    var result=document.querySelector("h3").innerHTML="PLAYER 2 Wins!🏆";
+}
+else if((randomNumber1==3)&&(randomNumber2==2)){
+    var result=document.querySelector("h3").innerHTML="PLAYER 1 Wins!🏆";
+}
+
+
+
